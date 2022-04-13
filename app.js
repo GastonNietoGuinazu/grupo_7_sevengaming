b62a6bd82596c936f547cbf0c86b8ce258370795
pedrocerebro <elcerebro@MacBook-Pro-de-pedro.local>
2022-04-13 17:38:36 -0300

    footer

---
+++ app.js
@@ -0,0 +1,14 @@
+const express=require('express');
+const app=express();
+const path=require("path")
+const request=require("http")
+
+app.use(express.static('public'));
+
+app.listen(3012,()=>{
+    console.log('servidor corriendo en http://localhost:3012');
+});
+
+app.get('/',(req,res)=>{
+    res.sendFile(__dirname+'/views/main.html')
+})
