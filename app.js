2df9e79b4f7208a58423e915f99bc5476c6830a9
rulso84 <javiermirandanieto@gmail.com>
2022-04-18 09:51:06 -0300

    Juntando todo

--- app.js
+++ app.js
@@ -9,11 +9,32 @@ app.listen(3012,()=>{
     console.log('servidor corriendo en http://localhost:3012');
 });
 
+app.use(express.static(path.resolve(__dirname, './public')));
+
 app.get('/',(req,res)=>{
-    res.sendFile(__dirname+'/views/main.html')
+    res.sendFile(__dirname+'/views/home.html')
 })
 
 app.get('/product-detail', function(req,res){
     res.sendFile(path.resolve(__dirname, './views/productDetail.html'));
+
+});
+
+app.get('/cuenta', function(req,res){
+        res.sendFile(path.resolve(__dirname, './views/cuenta.html'));
+
+});
+
+app.get('/producto', function(req,res){
+    res.sendFile(path.resolve(__dirname, './views/producto.html'));
+
+});
+
+app.get('/carrito', function(req,res){
+    res.sendFile(path.resolve(__dirname, './views/carrito.html'));
+
+});
+app.get('/login', function(req,res){
+    res.sendFile(path.resolve(__dirname, './views/login.html'));
 
 });
