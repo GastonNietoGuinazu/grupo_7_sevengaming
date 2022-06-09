ddce29a96664ad4ab1e0a76c6e65ae1405fd7806
bernarodriguez36 <bernarodriguez36@gmail.com>
2022-06-09 18:59:13 -0300

    Empezando login

--- models/User.js
+++ models/User.js
@@ -4,8 +4,21 @@ const User = {
     fileName: "./data/user.json",
     
     getData: function () {
-        return fs.readFileSync(this.fileName, "utf-8");
+        return JSON.parse(fs.readFileSync(this.fileName, "utf-8"));
     },
+    findAll: function () {
+        return this.getData();
+    },
+    findByPk: function (id){
+let allUsers = this.findAll();
+let userFound = allUsers.find(oneUser => onseUser.id === id);
+return userFound;
+    },
+    findByPk: function (id){
+        let allUsers = this.findAll();
+        let userFound = allUsers.find(oneUser => onseUser.id === id);
+        return userFound;
+            },
     create: function (userData) {
 
     }
