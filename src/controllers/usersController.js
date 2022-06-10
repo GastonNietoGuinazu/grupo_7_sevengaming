c4db1ccb271e45d7977c4d0ea163138c7168d335
bernarodriguez36 <bernarodriguez36@gmail.com>
2022-06-10 19:12:43 -0300

    Cambios login

--- src/controllers/usersController.js
+++ src/controllers/usersController.js
@@ -1,7 +1,7 @@
 const fs = require("fs");
 const path = require("path");
-const { check, validationResult } = require('express-validator');
-const bcrypt = require('bcrypt');   
+const { check, validationResult } = require("express-validator");
+const bcrypt = require("bcrypt");
 /* EXPORTANDO MODULO DE USUARIOS */
 /* const User = require('../models.User'); */
 
@@ -10,39 +10,39 @@ const usersFilePath = path.join(__dirname, "../data/user.json");
 /*const users = JSON.parse(fs.readFileSync(usersFilePath, 'utf-8'));*/
 
 const usersController = {
-
   login: (req, res) => {
     console.log(req.body);
-    
+
     res.render("login");
   },
   processLogin: function (req, res) {
     let errors = validationResult(req);
     if (errors.isEmpty()) {
-        let usersJson = fs.readFileSync('user.json', { errors: errors.errors })
-        let users;
-        if(usersJson == "") {
-            users = [];
-        } else {
-            users = JSON.parse(usersJson);
-        }
+      let usersJson = fs.readFileSync("user.json", { errors: errors.errors });
+      let users;
+      if (usersJson == "") {
+        users = [];
+      } else {
+        users = JSON.parse(usersJson);
+      }
 
-        for (let i = 0; i < users.length; i++) {
-           if(users[i].email == req.body.email) {
-            if(bcrypt.compareSync(req.body.password, users[i].password)) {
-                let usuarioLoguear = users[i];
-                break;
-            }
-           }
-
-           if(usuarioLoguear == undefined) {
-            return res.render('login', {errors: [
-                { msg: "Credenciales incorrectas"}
-            ]})
-           }
-            
+      for (let i = 0; i < users.length; i++) {
+        if (users[i].email == req.body.email) {
+          if (bcrypt.compareSync(req.body.password, users[i].password)) {
+            let usuarioLoguear = users[i];
+            return usuarioLoguear;
+          }
         }
-        req.session.usuarioLogueado = usuarioLoguear;
+        console.log(usuarioLoguear, "Prueba");
+
+        if (usuarioLoguear == undefined) {
+          return res.render("login", {
+            errors: [{ msg: "Credenciales incorrectas" }],
+          });
+        }
+      }
+      req.session.usuarioLogueado = usuarioLoguear;
+      res.redirect("/");
     } else {
       return res.render("login", { errors: errors.errors });
     }
--- src/routes/usersRouter.js
+++ src/routes/usersRouter.js
@@ -1,6 +1,6 @@
 const express = require("express");
 const router = express.Router();
-//const { check, validationResult } = require('express-validator');
+const { check, validationResult } = require('express-validator');
 
 const usersController = require("../controllers/usersController.js")
 
@@ -9,9 +9,9 @@ router.get("/cuenta", usersController.account);
 router.get("/registrarse", usersController.register)
 
 // Ruta de Login
-/*router.post('/login',[
+router.post('/login',[
 check('email'). isEmail().withMessage('Email inválido'),
 check('password'). isLength({min: 8}).withMessage('La contraseña debe tener al menos 8 caracteres')
-], usersController.processLogin)*/
+], usersController.processLogin)
 
 module.exports = router;
