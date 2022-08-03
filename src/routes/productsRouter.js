5f871b4563eaa1730ef77cb2bf182a55520fab96
GastonNietoGuinazu <gasjav97@gmail.com>
2022-08-03 14:19:47 -0300

    Arreglando detalles

--- src/routes/productsRouter.js
+++ src/routes/productsRouter.js
@@ -12,7 +12,7 @@ router.get("/carrito", productsController.carrito); //Vista de la selección de 
 router.get("/listaProducto", productsController.productList); //Vista de la lista de productos
 router.get("/detalle", productsController.productDetail); //Vista del detalle del producto
 router.get("/detail/:id", productsController.detail); //Detalle del producto, una alternativa de la vista de detalle
-router.get("crearProducto", productsController.crearProducto); //Formulario de creación de productos
+router.get("/crearProducto", productsController.crearProducto); //Formulario de creación de productos
 router.post("/crearProducto", productsController.store); //Crea y almacena el producto
 router.get("/editarProducto",uploadFile.single("imagen1"), productsController.editProduct) //Formulario de edición de productos
 router.post("/editarProducto/:id", productsController.edit); //Procesa la edición del producto
